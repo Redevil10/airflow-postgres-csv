dc427a01c1a2ac3e7267ef4f87a527f786bbe54a
Qing Wan <juveseason@gmail.com>
2026-02-08 20:50:18 +1100

    "v0.2.0: Breaking changes - consolidate sql param, add truncate & gzip

--- src/airflow_postgres_csv/__init__.py
+++ src/airflow_postgres_csv/__init__.py
@@ -3,4 +3,4 @@
 from airflow_postgres_csv.operators import CsvToPostgresOperator, PostgresToCsvOperator
 
 __all__ = ["PostgresToCsvOperator", "CsvToPostgresOperator"]
-__version__ = "0.1.0"
+__version__ = "0.2.0"
