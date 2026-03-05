9dc2016e5fcc38b09901b294b75afe1471dde466
Qing Wan <juveseason@gmail.com>
2026-03-06 10:17:56 +1100

    bump version to v0.4.0

--- src/airflow_postgres_csv/__init__.py
+++ src/airflow_postgres_csv/__init__.py
@@ -3,4 +3,4 @@
 from airflow_postgres_csv.operators import CsvToPostgresOperator, PostgresToCsvOperator
 
 __all__ = ["PostgresToCsvOperator", "CsvToPostgresOperator"]
-__version__ = "0.3.2"
+__version__ = "0.4.0"
